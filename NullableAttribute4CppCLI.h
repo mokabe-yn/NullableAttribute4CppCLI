89e5f38d67f2011aa5a8683119ec31e8f5fb69cd
okabe@home <okabe_m@hmi.aitech.ac.jp>
2021-05-11 06:53:08 +0900

    メインのファイル追加

---
+++ NullableAttribute4CppCLI.h
@@ -0,0 +1,57 @@
+﻿#pragma once
+// This code is referenced https://github.com/epsitec/nullable-type-reflection
+
+
+namespace System {
+namespace Runtime {
+namespace CompilerServices {
+
+
+[AttributeUsage(AttributeTargets::Class | AttributeTargets::Event | AttributeTargets::Field |
+    AttributeTargets::GenericParameter | AttributeTargets::Module | AttributeTargets::Parameter |
+    AttributeTargets::Property | AttributeTargets::ReturnValue,
+    AllowMultiple = false)]
+/// <summary>
+/// <para>
+/// コンパイラが認識するNullableアノテーションをCCLIから使用できるように、
+/// 明示的に<see cref="NullableAttribute"/>を実装します。
+/// C#コンパイラから参照されたくないのでアクセスレベルをinternalにしている。
+/// </para>
+/// </summary>
+public ref class NullableAttribute : Attribute {
+private:
+    using byte = unsigned __int8;
+    using arraybyte = array<byte>^;
+
+    byte _Mode;
+    arraybyte _Modes = nullptr;
+
+public:
+    property byte Mode {
+public:
+    byte get() { return _Mode; }
+private:
+    void set(byte value) { _Mode = value; }
+    }
+    property arraybyte Modes {
+public:
+    arraybyte get() { return _Modes; }
+private:
+    void set(arraybyte value) { _Modes = value; }
+    }
+
+public:
+    NullableAttribute(byte mode) {
+        Mode = mode;
+    }
+
+    NullableAttribute(arraybyte modes) {
+        Modes = modes;
+    }
+
+    //public NullableAttribute(params byte[] modes) => this.Modes = modes;
+
+};
+}
+}
+}
