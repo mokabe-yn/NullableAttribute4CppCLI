3461bfac795e97d7b5e3712ab771b3bd79f682a5
okabe@home <okabe_m@hmi.aitech.ac.jp>
2021-05-11 21:04:58 +0900

    NullableAttribute4CppCLIProbe追加

---
+++ NullableAttribute4CppCLIProbe/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace NullableAttribute4CppCLIProbe {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
